9b33505d50f0318ec638fb7374738daccbdcda75
icedog55puck <144482284+icedog55puck@users.noreply.github.com>
2023-11-07 22:09:07 +0000

    working calender sever

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const port = 3000;
@@ -7,14 +8,33 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile('public/index.html', { root: __dirname });
+const db = new sqlite3.Database('data.db');
+
+// Create a table to store appointment data
+db.serialize(() => {
+  db.run("CREATE TABLE IF NOT EXISTS appointments (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, date TEXT, time_slot TEXT)");
 });
 
-app.post('/submit', (req, res) => {
-  // Handle the input from the web page here
-  const inputData = req.body.input;
-  res.send(`Received input: ${inputData}`);
+// Insert a new appointment for a user
+app.post('/createAppointment', (req, res) => {
+  const { user_id, date, time_slot } = req.body;
+  db.run("INSERT INTO appointments (user_id, date, time_slot) VALUES (?, ?, ?)", [user_id, date, time_slot], function(err) {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.status(200).send('Appointment created successfully.');
+  });
+});
+
+// Retrieve user's appointments
+app.get('/userAppointments', (req, res) => {
+  const user_id = req.query.user_id;
+  db.all("SELECT * FROM appointments WHERE user_id = ?", [user_id], (err, rows) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.status(200).json(rows);
+  });
 });
 
 app.listen(port, () => {
